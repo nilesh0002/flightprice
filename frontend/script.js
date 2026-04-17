314800e4afd14512e5b0c08743a922e86c924c43
nilesh0002 <nilesh.singh2024@lpu.in>
2026-04-17 19:46:46 +0530

    do

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 
-const API_URL = "https://flightprice-sghf.onrender.com";
+const API_URL = "http://127.0.0.1:8000"; // Originally: "https://flightprice-sghf.onrender.com"
 let trendChart = null;
 
 // --- API Calls ---
