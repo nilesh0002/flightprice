b9a5027dcc3b2dce363ebb0a4566d9550085c2f7
nilesh0002 <nilesh.singh2024@lpu.in>
2026-04-18 03:12:54 +0530

    do

--- routes/chatRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const chatController = require('../controllers/chatController');
-
-router.post('/chat', chatController.getChatResponse);
-
-module.exports = router;
