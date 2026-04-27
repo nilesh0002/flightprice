c9b3081b85fa70484daa0acdc30c01f3c971bf14
nilesh0002 <nilesh.singh2024@lpu.in>
2026-04-27 16:01:18 +0530

    do

--- scratch/test_bot.py
+++ scratch/test_bot.py
@@ -3,5 +3,5 @@ import os
 sys.path.append(os.getcwd())
 from chatbot.rule_bot import get_chat_response
 
-response = get_chat_response("Delhi to Mumbai tomorrow")
-print(response)
+response = get_chat_response("i want to mumbai from delhi on date 29/04/2026")
+print(response.encode('utf-8').decode(sys.stdout.encoding, errors='replace'))
